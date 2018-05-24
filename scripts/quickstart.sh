398f08090b2d99bd316f67e756d91bfc6e2d728a
Matt Langbehn <matthew.langbehn@gmail.com>
2018-05-24 14:58:14 -0500

    Add minikube support
    We now are testing via minikube in CI. Additionally, now drupal can be
    run locally via minikube.
    
    Signed-off-by: Matt Langbehn <matthew.langbehn@gmail.com>

---
+++ scripts/quickstart.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+kubectl create -f kubernetes/local-volumes.yaml
+kubectl create -f kubernetes/postgres.yaml
+kubectl create -f kubernetes/drupal.yaml
+kubectl get nodes
+kubectl get svc drupal
